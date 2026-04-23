12498a6d3a13628cc1adddd064f6e0d643582bc0
Nourddine abatal <106854300+Nourddine90@users.noreply.github.com>
2026-04-23 17:25:53 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function calculate() {
-  let capital = document.getElementById("capital").value;
-  let profit = document.getElementById("profit").value;
-
-  let result = capital * (profit / 100);
-
-  document.getElementById("result").innerText =
-    "الربح المتوقع: " + result + " درهم";
-}
